6e07c3dca260c3b4e235045462c72cb0b829f4f4
cheriseba <cherise.barthel@stud-hawk.de>
2022-06-21 14:20:35 +0200

    zwischenspeicher

--- gametwo/gametwo.js
+++ gametwo/gametwo.js
@@ -1,12 +1,1 @@
-function setup () {
-
-    createCanvas(windowWidth,windowHeight);
-    background(128);
-    
-    }
-    
-    function draw () {
-    
-        rect(random(width),random(height),random(50),random(50));
-    
-    }
+s
